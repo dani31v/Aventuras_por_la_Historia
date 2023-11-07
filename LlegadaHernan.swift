c7720b1fca207a547c52ed3a144e1b99e3bac910
Dani Valencia <85707206+dani31v@users.noreply.github.com>
2023-11-07 17:06:09 -0600

    historiahernan

--- LlegadaHernan.swift
+++ LlegadaHernan.swift
@@ -25,6 +25,18 @@ struct LlegadaHernanView: View {
                     Image("Hernan")
                         .resizable()
                         .frame(width: 400, height: 400)
+                    Text("Cuando llegaron, Cortés hizo amigos con algunos indígenas que no estaban contentos con los gobernantes locales, especialmente un grupo llamado tlaxcaltecas. Estos amigos ayudaron a Cortés y se convirtieron en aliados importantes.")
+                        .font(.body)
+                        .foregroundColor(.black)
+                        .padding(.top, 40)
+                    Text("Hubo una batalla en un lugar llamado Centla en marzo de 1519, donde los amigos de Cortés pelearon contra algunos indígenas. Los españoles tenían armas avanzadas y ganaron la batalla. Después de la batalla, recibieron regalos, incluyendo una esclava llamada Malintzin, que también se llamaba La Malinche. Ella fue muy importante para Cortés porque podía hablar diferentes idiomas y le ayudó a entender a la gente de esa tierra nueva.")
+                        .font(.body)
+                        .foregroundColor(.black)
+                        .padding(.top, 40)
+                    Text("Así fue como Hernán Cortés y sus amigos comenzaron su aventura en México.")
+                        .font(.body)
+                        .foregroundColor(.black)
+                        .padding(.top, 35)
                     
                     Button(action: {
     
